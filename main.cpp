2eb4ad2e35f6e1488fd434a2de4071c8afe5aba8
tt--tt <tt-_-tt@bk.ru>
2013-09-09 20:54:51 +0400

    should be correct by now

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-const std::string HELLO_WORLD_MESSAGE = "Hello, world!!!";
+const std::string HELLO_WORLD_MESSAGE = "Hello, world!!!!!!!";
 
 int main() {
     std::cout << HELLO_WORLD_MESSAGE << std::endl;
